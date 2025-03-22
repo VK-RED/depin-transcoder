00b15dd4f03f8c730436d0649b329289d954561b
Vishnu Kumar <vishnukumart96@gmail.com>
2025-03-22 19:54:41 +0530

    fix: disable eslint rules

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,15 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+import { FlatCompat } from '@eslint/eslintrc'
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
+export default eslintConfig
